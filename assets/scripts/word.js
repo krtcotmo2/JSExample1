46caec2e4491558804ef8677e1403eaf7537d361
Kurt Cooney <krtcotmo2@gmail.com>
2018-11-27 23:06:09 -0500

    aded start to crossword puzzle

---
+++ assets/scripts/word.js
@@ -0,0 +1,14 @@
+class aWord{
+    constructor(xpos=0, ypos=0, val="", dir="h") {
+        this.xpos=xpos,
+        this.ypos=ypos,
+        this.dir=dir,
+        this.word=val,
+        this.sharedLetters= [],
+        this.sharedIndex = []
+    }
+  }
+
+
+
+
